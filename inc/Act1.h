2c3fd62f169d514a5c9698affdf26cedc13d4dda
265104 <singhruchi2798@gmail.com>
2021-04-30 20:29:57 +0530

    updated

---
+++ inc/Act1.h
@@ -0,0 +1,10 @@
+/**
+*@brief : The port function defined sets B0 to 1 clears D0 and D1 and finally sets them too.
+*/
+
+#ifndef ACTIVITY1_H_INCLUDED
+#define ACTIVITY1_H_INCLUDED
+
+void port();
+
+#endif // ACTIVITY1_H_INCLUDED
---
+++ inc/Act3.h
@@ -0,0 +1,7 @@
+#ifndef ACT3_H_INCLUDED
+#define ACT3_H_INCLUDED
+
+char OutPWM (uint16_t );
+void InitPWM();
+
+#endif // ACT3_H_INCLUDED
